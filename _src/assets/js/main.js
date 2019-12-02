e8f5526b8680683cd59c0c127f69832862d7ff05
Isabel del R <i.beldelreal@gmail.com>
2019-12-02 21:36:40 +0100

    Fixed function of Favorites

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-debugger */
 /* eslint-disable no-console */
 'use strict';
 
@@ -32,28 +33,21 @@ function createListshows(list) {
 function paintshows() {
   let htmlContent = '';
   let htmlFavorites = '';
+  for (let i = 0; i < showsFavorites.length; i++) {
+    htmlFavorites += `<li class="shows__item__fav" id="${[i]}">`;
+    htmlFavorites += `<div class="shows__container__fav">`;
+    htmlFavorites += `<img class="shows__container__fav--img" src="${showsFavorites[i].image}" title="${showsFavorites[i].name}" alt="${showsFavorites[i].name}">`;
+    htmlFavorites += `<h3 class="shows__container__fav--name">${showsFavorites[i].name}</h3>`;
+    htmlFavorites += `</div>`;
+    htmlFavorites += `</li>`;
+  }
   for (let i = 0; i < shows.length; i++) {
-    const isFavorite = showsFavorites.indexOf(i) !== -1;
-    if (isFavorite === true) {
-      console.log('Esta es mi lista:' + showsFavorites);
-      console.log('Esta es mi lista con index:' + showsFavorites.indexOf(i));
-      // htmlFavorites += `<li class="shows__item" id="${[i]}">`;
-      // htmlFavorites += `<div class="shows__container">`;
-      // htmlFavorites += `<img class="shows__container--img"src="${shows[i].image}" title="${shows[i].name}" alt="${shows[i].name}">`;
-      // htmlFavorites += `<h3 class="shows__container--name">${shows[i].name}</h3>`;
-      // htmlFavorites += `</div>`;
-      // htmlFavorites += `</li>`;
-
-    }
-
     htmlContent += `<li class="shows__item" id="${[i]}">`;
     htmlContent += `<div class="shows__container">`;
     htmlContent += `<img class="shows__container--img"src="${shows[i].image}" title="${shows[i].name}" alt="${shows[i].name}">`;
     htmlContent += `<h3 class="shows__container--name">${shows[i].name}</h3>`;
     htmlContent += `</div>`;
     htmlContent += `</li>`;
-
-
   }
   listFavorites.innerHTML = htmlFavorites;
   listShows.innerHTML = htmlContent;
@@ -61,36 +55,35 @@ function paintshows() {
 
 //This function is to check if the movie is favorite
 function toggleFavorites(ev) {
-
+  let findFav = false;
   const clickedId = parseInt(ev.currentTarget.id);
-  const favoriteIndex = showsFavorites.indexOf(clickedId);
-  const isFavorite = favoriteIndex !== -1;
-
-  if (isFavorite === true) {
-    console.log('Lo saco');
-    console.log('Favorite Index es:' + favoriteIndex + 'clicked Id es: ' + clickedId);
-    //showsFavorites.splice(favoriteIndex, 1);
-    //showsFavorites[showsFavorites.length];
+  if (showsFavorites.length === 0) {
+    showsFavorites.push({ id: 0, image: `${shows[clickedId].image}`, name: `${shows[clickedId].name}` });
+    console.log('Se ha añadido en Favs 0');
   }
+
   else {
-    for (let i = -1; i < showsFavorites.length; i++) {
-      showsFavorites[showsFavorites.length] = {
-        image: shows[clickedId].image,
-        name: shows[clickedId].name,
-        id: showsFavorites[showsFavorites.length]
-      };
-    }
-    //showsFavorites.push(showsFavorites[showsFavorites.length]);
-    console.log('Está metido');
-    console.log('Favorite Index es:' + favoriteIndex + 'clicked Id es: ' + clickedId);
-  }
+    for (let i = 0; i < showsFavorites.length; i++) {
+      if (shows[clickedId].name === showsFavorites[i].name) {
+        showsFavorites.splice(i, 1);
+        console.log('Se ha eliminado: ' + i + 'Favoritos es: ' + showsFavorites);
+        findFav = true;
+        console.log('Ha encontrado a favorito');
+      }
 
+    }
+    if (findFav === false) {
+      showsFavorites.push({ id: showsFavorites.length, image: `${shows[clickedId].image}`, name: `${shows[clickedId].name}` });
+      console.log('Favorito se ha añadido');
+    }
+  }
 
   console.log('La longitud es ' + showsFavorites.length);
   console.log('Los favs son: ' + showsFavorites);
-  //paintshows();
+  paintshows();
   listenshows();
 }
+
 
 //This function is going to listen in case you click a movie
 function listenshows() {
